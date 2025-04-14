15d26e82b386e101e95b68415d5923176f8dedb4
jodeon <deon_jsn@hotmail.com>
2025-04-14 14:51:00 -0300

    Teste READ

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import requests
 BASE_URL = 'http://127.0.0.1:5000'
 tasks = []
 
+# Test CREATE
 def test_create_fask():
     new_task_data = {
         "title": "Nova tarefa",
@@ -17,3 +18,21 @@ def test_create_fask():
     assert "message" in response_json
     assert "id" in response_json
     tasks.append(response_json['id'])
+
+# Test READ
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "task" in response_json
+    assert "total_tasks" in response_json
+
+# Test READ expecifico
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
+        
